05a1fe6ade9c50f67c625532397d121be5ec09d1
LeeDongHyeuk <deeprogrammer404@gmail.com>
2022-01-27 23:56:02 +0900

    2022.01.27

--- Baekjoon/16194.cpp
+++ Baekjoon/16194.cpp
@@ -7,17 +7,23 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<int> a(n + 1);
-	
+	vector<int> p(n + 1);
+
 	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
+		cin >> p[i];
 	}
 
 	vector<int> d(n + 1);
 
 	for (int i = 1; i <= n; i++) {
+		d[i] = 1000 * 10000;
+	}
+
+	d[0] = 0;
+
+	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
-			d[i] = max(d[i], d[i - j] + a[j]);
+			d[i] = min(d[i], d[i - j] + p[j]);
 		}
 	}
 
