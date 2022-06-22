39756a62f3d646f485b3274c6f208ba7e5cf66ac
LeeDongHyeuk <deeprogrammer404@gmail.com>
2022-06-23 01:02:20 +0900

    2022.06.23

---
+++ Baekjoon/2422.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+bool a[201][201];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m;
+	cin >> n >> m;
+
+	while (m--) {
+		int u, v;
+		cin >> u >> v;
+		a[u][v] = a[v][u] = true;
+	}
+
+	int ans = 0;
+	for (int i = 1; i <= n - 2; i++) {
+		for (int j = i + 1; j <= n - 1; j++) {
+			for (int k = j + 1; k <= n; k++) {
+				if (a[i][j] || a[j][k] || a[k][i]) continue;
+				ans++;
+			}
+		}
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
